beec67f2704768835ca31e80b8e7c4365e8e9fb1
Kyle Dove <kddove85@gmail.com>
2014-08-03 23:07:47 -0400

    Question
    Aug 03 2014    Kyle Dove            Developed technique to pull question
    from page source using
    regex
    Aug 03 2014    Greg Saxton          Developed better technique to pull
    question from page source
    using CSS Selector

--- SantaClara5.py
+++ SantaClara5.py
@@ -14,11 +14,16 @@ Jul 28 2014    Kyle Dove            Created
 Jul 29 2014    Kyle Dove            Removed credentials from hardcode and placed in separate file
                                     Added logic to advance to the questions page (past the modal 
                                     and past the questions link, to grandmother's house we go)
-                                    
+Aug 03 2014    Kyle Dove            Developed technique to pull question from page source using
+                                    regex
+Aug 03 2014    Greg Saxton          Developed better technique to pull question from page source
+                                    using CSS Selector
 '''
 
 from selenium import webdriver
 import time
+import re
+
 
 #chromeDriverPath = "C:/ChromeBinary/chromedriver.exe"
 #driver = webdriver.Chrome(chromeDriverPath)
@@ -26,6 +31,23 @@ import time
 separator=":"
 fileIN = open('creds', "r")
 line = fileIN.readline()
+
+qtxt = "qtext_"
+
+#Working... kind of...
+#Don't fucking touch this...
+#regex = '<div class="qtext" id="qtext_(.*)>\s<p>(.+?)<\/p>\s<\/div>'
+#pattern = re.compile(regex)
+
+#Working... kind of...
+#Don't fucking touch this...
+#regex = '<div class="qtext" id="qtext_(.*)>\s<p>(.+?)<\/p>\s<\/div>'
+#pattern = re.compile(regex)
+
+#Working... kind of...
+#Don't fucking touch this...
+regex = '<div class="qtext" id="qtext_(.*.)>\s<p>(.+?)<\/p>\s<\/div>'
+pattern = re.compile(regex)
 
 while line:
     sout=line.split(separator)
@@ -68,15 +90,33 @@ time.sleep(10)
 
 #Fucking Please!!! Why Mega Man X is so fucking good that it makes my dick rock hard
 #Click the link to questions
-driver.find_element_by_xpath("//div[@id='main_content']/div[2]/div[1]/ul[1]/li[3]/a").click()
+#driver.find_element_by_xpath("//div[@id='main_content']/div[2]/div[1]/ul[1]/li[3]/a").click()
+
+#Greg's way of clicking the link
+driver.find_element_by_css_selector("#pane_new > div.links > ul > li > a").click()
 
 time.sleep(10)
 
 #Ridin' on Cars!!!
 #Click the accept button on the modal to advance to the questions page
-driver.find_element_by_xpath("//div[@id='questions_intro']/a[1]").click()
+#driver.find_element_by_xpath("//div[@id='questions_intro']/a[1]").click()
 
-#driver.switch_to.alert.accept()
+#Greg's way of clicking the link
+driver.find_element_by_css_selector("#questions_intro > div.buttons > ul > li > button").click()
+
+print(driver.find_element_by_xpath("//div[@id='new_question']"))
+
+print driver.page_source
+
+question = re.search(pattern,driver.page_source)
+print(question)
+
+question = re.findall(pattern,driver.page_source)
+print(question)
+
+#Greg's way of getting question and printing it
+question = driver.find_element_by_css_selector("div[id^="+qtxt+"]").text
+print(question)
 
 #Close Down
 #driver.close()
